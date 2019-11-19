f9217b17c7106834bccaf99fd6396bc9ae29c8cc
Prudo LODONOU <soycaindevit@gmail.com>
2019-11-19 14:52:59 +0000

    refactor : delete unused variable start_time in load_data function

--- bikeshare.py
+++ bikeshare.py
@@ -272,7 +272,7 @@ def user_stats(df):
     """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
-    start_time = time.time()
+ 
 
     # Display counts of user types
     user_types = df['User Type'].value_counts()
